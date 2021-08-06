a15217d91aa0a2f4b188bf84416717b7168632ac
Mayra <mayra.diaz@utec.edu.pe>
2021-08-06 14:08:48 -0500

    Count files

--- util.py
+++ util.py
@@ -1,3 +1,4 @@
+import os
 
 def get_point(vector):
     doble = vector + vector
@@ -5,3 +6,10 @@ def get_point(vector):
 
 def euclidean_distance(a, b):
     return sum([(a[i]-b[i])**2 for i in range(len(a))])**0.5
+
+def get_number_of_files(directory):
+    files_n = 0
+    for base, dirs, files in os.walk(directory):
+        for f in files:
+            files_n += 1
+    return files_n
